e45d303b2b191dc392fad138bbfa594e2dcfb043
kiRiLL3311 <1632113@mail.ru>
2026-07-03 14:30:24 -0700

    broken

---
+++ auth/go.mod
@@ -0,0 +1,9 @@
+module github.com/kiRiLL3311/Currency/auth
+
+go 1.26.4
+
+require (
+	github.com/golang-jwt/jwt/v5 v5.3.1 // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
+	golang.org/x/crypto v0.53.0 // indirect
+)
--- backend/migrations/20260625233125_create_rates_table.up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE rates (
-    id SERIAL PRIMARY KEY,
-    base_currency VARCHAR(10) NOT NULL,
-    target_currency VARCHAR(10) NOT NULL,
-    rate DECIMAL(10,6) NOT NULL,
-    created_at TIMESTAMP DEFAULT NOW()
-);
